23ca7c7502f58912447faddf546052ce5beb0267
Khoi Nguyen <minhkhoi632000@gmail.com>
2020-05-18 17:15:52 -0700

    Add latest update, working on the moving for Iceman

--- IceMan/Actor.cpp
+++ IceMan/Actor.cpp
@@ -1,119 +1,185 @@
 #include "Actor.h"
+#include "GameController.h"
 #include "StudentWorld.h"
+#include <cmath>
 using namespace std;
 
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
 
-unique_ptr<IActorResponse> Actor::collide (shared_ptr<Actor>& receiver) {
 
-	if (this->type == player) {
+//Destroy an object or deal damage to characters
+void Destroy::response() {
+	if(target->isAlive())
+		target->dmgActor(dmgTaken);
+}
+
+void FreeMovement::moveThatAss() {
+}
+
+void FallMovement::moveThatAss() {
+}
+
+
+void IceMan::doSomething() {
+	if (!isAlive())
+		return;
+}
+
+void Protesters::shout(Direction dir) {
+}
+
+
+
+void Protesters::doSomething() {
+}
+
+
+std::shared_ptr<Actor> RadarLikeDetection::sensedActor() {
+	/*****************************
+	Check if the player is in certain radius of the actor
+	Check in all direction, that means using a circle and Euclidean distance math, the detection range for the actor and the actor
+	Use Euclidean to find out the distance from the player and the actor
+	sum of detection range of the player and the actor is the "spot zone"
+	If the distance is smaller or equal than the "spot zone"
+		|
+		Then return the intruder, the player is "sensed"
+	If it's not then return nullptr
+*****************************/
+	int distance = sqrt(pow(source->getX() - intruder->getX(), 2) + pow(source->getY() - intruder->getY(), 2));
+	int spotZone = this->range + intruder->getDetectRange();
+	if (distance <= spotZone)
+		return intruder;
+	return nullptr;
+}
+
+void RadarLikeDetection::behaveBitches() {
+	sensedActor();
+}
+
+
+void CollisionDetection::behaveBitches() {
+	collisionHappen();
+}
+
+bool LineOfSightDetection::seePlayer() {
+	/******************************
+Check if the player is in the horizontal or vertical direction of the actor
+Eye sight is blocked by worldStatic, ice actor type
+Check the vertical direction first
+	Check eyesight from the position of the actor, if an actor block or get out of screen before reaching the player then return false
+	If found the player then turn the actor toward the characters direction and return false
+		Get X distance between the actor and the player
+		If it's the same
+				|
+				Calculate the Y distance between the actor and the player (absolute value)
+				Check if there's any ice or worlsStatic actor in the X location that has Y location smaller than the distance
+					If there's smaller -> There's something blocking the view -> false
+					If none -> see the player -> true
+Same for horizontal direction
+*******************************/
+	return false;
+}
+
+void LineOfSightDetection::behaveBitches() {
+	seePlayer();
+}
+
+
+void CollisionDetection::collide(std::shared_ptr<Actor>& source, std::shared_ptr<Actor>& receiver) {
+	if (source->type == Actor::player) {
 		switch (receiver->type) {
-		case Actor::worldStatic: {
-			unique_ptr<Block> re = make_unique<Block>(this);
-			return re;
-			}
+		case Actor::worldStatic:
+			source->collisionResult = make_unique<Block>(source);
+			break;
 		case Actor::hazard: {
-			unique_ptr<Destroy> re = make_unique<Destroy>(this, receiver->getStrength());
-			return re;
+			source->collisionResult = make_unique<Destroy>(source, receiver->getStrength());
+			break;
 		}
 		default:
 			break;
 		}
 	}
-	if (this->type == npc) {
+	if (source->type == Actor::npc) {
 		switch (receiver->type) {
-		case Actor::hazard:{
-			unique_ptr<Destroy> re = make_unique<Destroy>(this, receiver->getStrength());
-			return re;
-		}
-		case Actor::worldStatic:
-		case Actor::ice: {
-			unique_ptr<Block> re = make_unique<Block>(this);
-			return re;
-		}
+		case Actor::hazard:
+			source->collisionResult = make_unique<Destroy>(source, receiver->getStrength());
+			break;
+		case Actor::Actor::worldStatic:
+		case Actor::ice:
+			source->collisionResult = make_unique<Block>(source);
+			break;
 		default:
 			break;
 		}
 	}
 	//World static doesn't count because it's never move or destroy
 
-	if (this->type == hazard) {
+	if (source->type == Actor::hazard) {
 		switch (receiver->type) {
-		case Actor::worldStatic:
-		case Actor::npc:
+		case Actor::Actor::worldStatic:
+		case Actor::Actor::npc:
 		case Actor::player:
-		case Actor::ice:{
-			unique_ptr<Destroy> re = make_unique<Destroy>(this, receiver->getStrength());
-			return re;
-		}
+		case Actor::ice:
+			source->collisionResult = make_unique<Destroy>(source, receiver->getStrength());
+			break;
 		default:
 			break;
 		}
 	}
-	if (this->type == ice) {
+	if (source->type == Actor::ice) {
 		switch (receiver->type) {
-		case Actor::player:{
-			unique_ptr<Destroy> re = make_unique<Destroy>(this, receiver->getStrength());
-			return re;
-		}
+		case Actor::player:
+			source->collisionResult = make_unique<Destroy>(source, receiver->getStrength());
+			break;
 		default:
 			break;
 		}
 	}
-	if (this->type == dropByPlayer) {
+	if (source->type == Actor::dropByPlayer) {
 		switch (receiver->type) {
-		case Actor::npc:{
-			unique_ptr<Destroy> re = make_unique<Destroy>(this, receiver->getStrength());
-			return re;
-		}
+		case Actor::npc:
+			source->collisionResult = make_unique<Destroy>(source, receiver->getStrength());
+			break;
 		default:
 			break;
 		}
 	}
-	if (this->type == collect) {
+	if (source->type == Actor::collect) {
 		switch (receiver->type) {
-		case Actor::player:{
-			unique_ptr<Destroy> re = make_unique<Destroy>(this, receiver->getStrength());
-			return re;
-		}
+		case Actor::player:
+			source->collisionResult = make_unique<Destroy>(source, receiver->getStrength());
+			break;
 		default:
 			break;
 		}
 	}
-	return nullptr;
 }
 
-
-void Actor::doSomething() {
-}
-
-void Boulder::falling() {
-}
-
-bool Boulder::checkIceBelow() {
+//Collision is just a radar like detection but only cover a small radius
+bool CollisionDetection::collisionHappen() {
+/*****************************
+See if you can "sensed" the actor. **This use an entirely different detection range than the "radar" detection range so it's ok**
+Then a collision happen and you should produce a collision result
+*****************************/
+	if (source->isAlive() && intruder->isAlive()) {
+		if (sensedActor())
+			collide(source, intruder);
+		else 
+			return false;
+		if (source->collisionResult) {
+			source->collisionResult->response();
+			return true;
+		}
+	}
 	return false;
 }
 
-void Boulder::doSomething() {
+
+void Block::response() {
 }
 
-
-int Squirt::getAmmo() {
-	return 0;
+void Collectable::showSelf() {
 }
-
-void Squirt::increaseAmmo(int amount) {
-}
-
-void Squirt::shoot() {
-}
-
-void Squirt::doSomething() {
-}
-
-void Ice::doSomething() {
-}
-
 
 void OilBarrels::doSomething() {
 }
@@ -126,12 +192,6 @@ void GoldNuggets::drop() {
 }
 
 void GoldNuggets::doSomething() {
-}
-
-void ExistTemporary::killMeNow() {
-}
-
-void Collectable::showSelf() {
 }
 
 int SonarKit::getAmmo() {
@@ -151,49 +211,61 @@ void SonarKit::doSomething() {
 void Water::doSomething() {
 }
 
-void Block::response() {
+
+void Squirt::increaseAmmo(int amount) {
 }
 
-//Destroy an object or deal damage to characters
-void Destroy::response() {
-	if(target->isAlive())
-		target->dmgActor(dmgTaken);
+void Squirt::shoot() {
 }
 
-void FreeMovement::moveThatAss() {
+void Squirt::doSomething() {
 }
 
-void FallMovement::moveThatAss() {
+void Boulder::fall() {
 }
+
+bool Boulder::checkIceBelow() {
+	return false;
+}
+
+void Boulder::doSomething() {
+}
+
+void Ice::doSomething() {
+}
+
+void ExistTemporary::showYourself() {
+}
+
+void ExistPermanently::showYourself() {
+}
+
+
 
 void ControlledMovement::moveThatAss() {
-}
-
-void PursuingMovement::moveThatAss() {
-}
-
-DetectPlayerBehavior::DetectPlayerBehavior(bool radarEnable) {
-}
-
-void DetectPlayerBehavior::behaveBitches() {
-}
-
-void DetectInanimateBehavior::behaveBitches() {
-}
-
-void IceMan::doSomething() {
-}
-
-void Protesters::shout(Direction dir) {
-}
-
-
-
-void Protesters::doSomething() {
-}
-
-IDetectionBehavior::IDetectionBehavior(bool radarEnable) {
-}
-
-void SquirtMovement::moveThatAss() {
+	/****************************
+	This tells where the character that being controlled should move
+	First you have to turn the character to face the direction you move first
+	If the characters already facing the direction you move, then move it toward that direction 1 square
+	If the move destination ends up with a collide block, activate the Block behavior
+	If the move destination ends up with an ice or collectible, activate the Destroy behavior
+	*****************************/
+	if (key != INVALID_KEY) {
+		switch (key) {
+		case KEY_PRESS_DOWN:
+			if (pawn->getDirection() != GraphObject::Direction::down) {
+				pawn->setDirection(GraphObject::Direction::down);
+			}
+			else {
+				if (!pawn->collisionDetection) {
+					//This is bad, i should check the coordinate instead of making another actors to check it for me. But fuck it
+					
+					pawn->collisionDetection = make_unique<CollisionDetection>(pawn, pawn->getCollisionRange())
+				}
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
--- IceMan/Actor.h
+++ IceMan/Actor.h
@@ -2,12 +2,87 @@
 #define ACTOR_H_
 
 #include <iostream>
+#include "StudentWorld.h"
 #include "GraphObject.h"
-
+#include <vector>
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class IMovementBehavior;
+class IActorResponse;
+class IDetectionBehavior;
+class RadarLikeDetection;
+class SonarKit;
+class GoldNuggets;
+class Squirt;
+class Boulder;
+
+
+
+class Actor : public GraphObject {
+public:
+	//Type
+	enum ActorType { player, npc, worldStatic, hazard, ice, dropByPlayer, collect };
+	ActorType type;
+	
+private:
+	int hitpoints;
+	int strength;
+	int collisionRange;
+	int detectionRange;
+	StudentWorld* m_sw;
+
+public:
+	Actor(StudentWorld*& world, ActorType type, bool visibility, int imgID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0, int t_hp = 1, int t_strength = 0, int col_range = 0, int detect_range = 0) : GraphObject(imgID, startX, startY, dir, size, depth), hitpoints(t_hp), strength(t_strength), collisionRange(col_range), detectionRange(detect_range), type(worldStatic), m_sw(world) {
+		setVisible(visibility);
+	};
+	virtual ~Actor() {};
+
+
+	std::unique_ptr<IMovementBehavior> movementBehavior;
+	//Store the result in this
+	std::unique_ptr<IActorResponse> collisionResult;
+	//Determine what method of detection the actor would use
+	std::unique_ptr<IDetectionBehavior>detectBehavior;
+	std::unique_ptr<IDetectionBehavior>collisionDetection;
+
+	StudentWorld* getWorld() {
+		return m_sw;
+	}
+
+	bool isAlive() {
+		return hitpoints > 0;
+	}
+
+	//Don't use it before derived
+	virtual void doSomething() = 0;
+
+	int getDetectRange() {
+		return detectionRange;
+	}
+
+	int getCollisionRange() {
+		return collisionRange;
+	}
+
+	int getHealth() {
+		return hitpoints;
+	}
+
+	int getStrength() {
+		return strength;
+	}
+
+	void dmgActor(int amt) {
+		hitpoints -= amt;
+	}
+
+	void changeActorType(ActorType t_type) {
+		type = t_type;
+	}
+};
+
 //Movement Strategy
-class IMovementBehavior{
+class IMovementBehavior {
 public:
 	virtual void moveThatAss() = 0;
 };
@@ -30,8 +105,13 @@ public:
 
 class ControlledMovement : public IMovementBehavior{
 private:
-
+	int key;
+	std::shared_ptr<Actor> pawn;
 public:
+	ControlledMovement(std::shared_ptr<Actor> t_pawn) : pawn(t_pawn) {
+		if (!pawn->getWorld()->getKey(key))
+			key = INVALID_KEY;
+	}
 	//This is for the player
 	void moveThatAss() override;
 };
@@ -62,19 +142,19 @@ public:
 
 class Block : public IActorResponse {
 private:
-	shared_ptr<Actor>target;
+	std::shared_ptr<Actor>target;
 public:
-	Block(shared_ptr<Actor>& t_target) : target(t_target) {}
+	Block(std::shared_ptr<Actor>& t_target) : target(t_target) {}
 	//Object will be force to stand still
 	void response() override;
 };
 
 class Destroy : public IActorResponse {
 private:
-	shared_ptr<Actor>target;
+	std::shared_ptr<Actor>target;
 	int dmgTaken;
 public:
-	Destroy(shared_ptr<Actor>& t_target, int dmgTook) : target(t_target), dmgTaken(dmgTook) {};
+	Destroy(std::shared_ptr<Actor>& t_target, int dmgTook) : target(t_target), dmgTaken(dmgTook) {};
 	//Object will be force to reduce their health by an amount
 	void response() override;
 };
@@ -93,117 +173,115 @@ class ExistTemporary : public IExistenceBehavior{
 private:
 	int timeTillDeath;
 public:
-	void killMeNow();
+	void showYourself() override;
 };
 
 class ExistPermanently : public IExistenceBehavior{
 private:
 	int lifeIsAPain;
 public:
+	void showYourself() override;
 };
 
 /////////////////////////
 //Detection Strategy
+//Collision detection and radar detection are the same type, just have different range
 class IDetectionBehavior{
 protected:
-	bool radarEnable;
-	int radarRange;
-	bool inLineOfSight;
+	std::shared_ptr<Actor> source, intruder;
 public:
-	//Function see if the actor should have the radar detect like feature on or off
-	IDetectionBehavior(bool radarEnable = true);
+	//This is a bad idea, but i blame the due date
 	virtual void behaveBitches() = 0;
+	std::shared_ptr<Actor> getSource() {
+		return source;
+	}
+	std::shared_ptr<Actor> getIntruder() {
+		return intruder;
+	}
+	void setSource(std::shared_ptr<Actor>& t_source) {
+		source.reset();
+		source = t_source;
+	}
+	void setIntruder(std::shared_ptr<Actor>& t_intruder) {
+		intruder.reset();
+		intruder = std::move(t_intruder);
+	}
 };
 
-class DetectPlayerBehavior : public IDetectionBehavior{
+class LineOfSightDetection : public IDetectionBehavior {
 private:
-
+	std::shared_ptr<Actor> source, intruder;
+	bool seePlayer();
 public:
-	DetectPlayerBehavior(bool radar) : IDetectionBehavior(radar) {};
-	//What the character do when detect the player
-	virtual void behaveBitches() override;
+	LineOfSightDetection(std::shared_ptr<Actor> t_source, std::shared_ptr<Actor>t_intruder) : source(t_source), intruder(t_intruder) {};
+	void behaveBitches() override;
 };
 
-class DetectInanimateBehavior : public IDetectionBehavior{
-private:
-
+class RadarLikeDetection : public IDetectionBehavior {
+protected:
+	int range;
+	//This is the location of the npc
+	std::shared_ptr<Actor> source;
+	//This will be the player
+	std::shared_ptr<Actor> intruder;
+	std::shared_ptr<Actor> sensedActor();
 public:
-	DetectInanimateBehavior() : IDetectionBehavior(true) {};
-	//What the character do when detect an inanimate object
-	virtual void behaveBitches() override;
+	RadarLikeDetection(std::shared_ptr<Actor>& t_source, int t_range) : source(t_source), range(t_range) {};
+	int getRange() {
+		return range;
+	}
+	void behaveBitches() override;
+};
+
+class CollisionDetection : public RadarLikeDetection {
+private:
+	//Calculate the collision result base on the type that collided into
+	void collide(std::shared_ptr<Actor>& source, std::shared_ptr<Actor>& receiver);
+	bool collisionHappen();
+public:
+	CollisionDetection(std::shared_ptr<Actor>& t_source, int t_range, std::shared_ptr<Actor>& t_intruder) : RadarLikeDetection(t_source, t_range, t_intruder){}
+	void behaveBitches() override;
 };
 
 /////////////////////////
 
-class Actor : public GraphObject{
-private:
-	int hitpoints;
-	int strength;
-
-protected:
-	//Type
-	enum ActorType { player, npc, worldStatic, hazard, ice, dropByPlayer, collect };
-	ActorType type;
-
-	std::unique_ptr<IMovementBehavior> movementBehavior;
-	//Calculate the collision result base on the type that collided into
-	std::unique_ptr<IActorResponse> collide(shared_ptr<Actor>& receiver);
-	//Store the result in this
-	std::unique_ptr<IActorResponse> collisionResult;
-
-public:
-	Actor(ActorType type, bool visibility, int imgID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0, int t_hp = 1, int t_strength = 0) : GraphObject(imgID, startX, startY, dir, size, depth), hitpoints(t_hp), strength(t_strength){
-		setVisible(visibility);
-	};
-	virtual ~Actor() {};
-
-	bool isAlive() {
-		return hitpoints > 0;
-	}
-
-	//Don't use it before derived
-	virtual void doSomething() = 0;
-
-	int getStrength() {
-		return strength;
-	}
-
-	void dmgActor(int amt) {
-		hitpoints -= amt;
-	}
-
-	void changeActorType(ActorType t_type) {
-		type = t_type;
-	}
-};
 
 //People
 class Characters : public Actor {
 protected:
-	std::unique_ptr<IDetectionBehavior>detectBehavior;
+	std::unique_ptr<IDetectionBehavior>characterCollisionBehavior;
 public:
-	Characters(ActorType type, int imgID, int startX, int startY, Direction dir, int t_hp, int t_str = 0) : Actor(type, true, imgID, startX, startY, dir, 1.0, 0, t_hp, t_str) {};
+	Characters(StudentWorld*& world, ActorType type, int imgID, int startX, int startY, Direction dir, int t_hp, int t_str, int col_ran, int detect_range) : Actor(world, type, true, imgID, startX, startY, dir, 1.0, 0, t_hp, t_str, col_ran, detect_range) {};
 	virtual ~Characters() {};
 };
 
 class IceMan : public Characters{
 private:
-	
+	std::vector<std::shared_ptr<SonarKit>>sonarVec;
+	std::vector<std::shared_ptr<GoldNuggets>>goldVec;
+	std::vector<std::shared_ptr<Squirt>>squirtVec;
 public:
-	IceMan(int startX = 30, int startY = 60) : Characters(player, IID_PLAYER, startX, startY, right, 10, 1) {
+	IceMan(StudentWorld*& world, int startX = 30, int startY = 60) : Characters(world, player, IID_PLAYER, startX, startY, right, 10, 1, 4, 12) {
 		movementBehavior = std::make_unique<ControlledMovement>();
-		detectBehavior = std::make_unique<DetectInanimateBehavior>(true);
 	};
 	void doSomething() override;
+	int getSonarNum() {
+		return sonarVec.size();
+	}
+	int getSquirtNum() {
+		return squirtVec.size();
+	}
+	int getGoldNum() {
+		return goldVec.size();
+	}
 };
 
 class Protesters : public Characters{
 private:
 	bool outOfField = false;
 public:
-	Protesters(int imgID = IID_PROTESTER, int startX = 60, int startY = 60, int hp = 5, int t_str = 2) : Characters(npc, imgID, startX, startY, left, hp, t_str){
+	Protesters(StudentWorld*& world, int imgID = IID_PROTESTER, int startX = 60, int startY = 60, int hp = 5, int t_str = 2, int col_range = 4, int detect_range = 0) : Characters(world, npc, imgID, startX, startY, left, hp, t_str, col_range, detect_range){
 		movementBehavior = std::make_unique<FreeMovement>();
-		detectBehavior = std::make_unique<DetectPlayerBehavior>(false);
 	}
 	//Functions
 	void rest() {
@@ -221,7 +299,6 @@ public:
 	void bribed() {
 		//Pursuing toward the exit
 		movementBehavior = std::make_unique<PursuingMovement>();
-		detectBehavior.reset();
 	}
 	void doSomething() override;
 };
@@ -230,9 +307,8 @@ class HardcoreProtesters: public Protesters{
 private:
 
 public:
-	HardcoreProtesters(int startX = 60, int startY = 60, int hp = 20, int strength = 2) : Protesters(IID_HARD_CORE_PROTESTER, startX, startY, hp, strength) {
+	HardcoreProtesters(StudentWorld*& world, int startX, int startY, int hp, int strength, int col_range, int detect_range) : Protesters(world, IID_HARD_CORE_PROTESTER, startX, startY, hp, strength, col_range, detect_range) {
 		movementBehavior = std::make_unique<FreeMovement>();
-		detectBehavior = std::make_unique<DetectPlayerBehavior>(true);
 	}
 };
 
@@ -241,7 +317,7 @@ class Inanimated : public Actor {
 private:
 
 public:
-	Inanimated(ActorType t_type, bool visibility, int imgID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 2, int hp = 1, int strength = 0) : Actor(t_type, visibility, imgID, startX, startY, dir, size, depth, hp, strength) {};
+	Inanimated(StudentWorld*& world, ActorType t_type, bool visibility, int imgID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 2, int hp = 1, int strength = 0, int col_range = 4, int detect_range = 0) : Actor(world, t_type, visibility, imgID, startX, startY, dir, size, depth, hp, strength, col_range, detect_range) {};
 
 	virtual ~Inanimated() {};
 };
@@ -252,7 +328,7 @@ private:
 protected:
 	std::unique_ptr<IExistenceBehavior> existBehavior;
 public:
-	Collectable(bool visibility, int imgID, int startX, int startY) : Inanimated(collect, visibility, imgID, startX, startY), isHidden(!visibility) {};
+	Collectable(StudentWorld*& world, bool visibility, int imgID, int startX, int startY, Direction dir, double size, unsigned int depth, int hp, int strength, int col_range, int detect_range) : Inanimated(world, collect, visibility, imgID, startX, startY, dir, size, depth, hp, strength, col_range, detect_range), isHidden(!visibility) {};
 	virtual ~Collectable() {};
 	void showSelf();
 };
@@ -263,7 +339,7 @@ private:
 	//Functions
 	void doSomething() override;
 public:
-	OilBarrels(int startX, int startY) : Collectable(false, IID_BARREL, startX, startY) {
+	OilBarrels(StudentWorld*& world, int startX, int startY, Direction dir = right, double size = 1.0, unsigned depth = 2.0, int hp = 1, int strength = 0, int col_range = 3, int detect_range = 4) : Collectable(world, false, IID_BARREL, startX, startY, dir, size, depth, hp, strength, col_range, detect_range) {
 		existBehavior = std::make_unique<ExistPermanently>();
 	};
 
@@ -276,7 +352,7 @@ private:
 	//Determine if the time for the Temporary gold exist ran out
 	bool tempTimeEnd();
 public:
-	GoldNuggets(int startX, int startY, bool t_pickable = true) : Collectable(false, IID_GOLD, startX, startY), pickableByPlayer(t_pickable) {
+	GoldNuggets(StudentWorld*& world, int startX, int startY, bool t_pickable = true, Direction dir = right, double size = 1.0, unsigned depth = 2.0, int hp = 1, int strength = 0, int col_range = 3, int detect_range = 4) : Collectable(world, false, IID_GOLD, startX, startY, dir, size, depth, hp, strength, col_range, detect_range), pickableByPlayer(t_pickable) {
 		if(pickableByPlayer)
 			existBehavior = std::make_unique<ExistPermanently>();
 		else {
@@ -297,7 +373,7 @@ private:
 	int increaseAmmo(int amount);
 	void useSonar();
 public:
-	SonarKit(int startX = 0, int startY = 60) : Collectable(true, IID_SONAR, startX, startY) {
+	SonarKit(StudentWorld*& world, int startX = 0, int startY = 60, Direction dir = right, double size = 1.0, unsigned depth = 2.0, int hp = 1, int strength = 0, int col_range = 3, int detect_range = 9999) : Collectable(world, true, IID_SONAR, startX, startY, dir, size, depth, hp, strength, col_range, detect_range) {
 		existBehavior = std::make_unique<ExistTemporary>();
 	};
 	void doSomething() override;
@@ -306,7 +382,7 @@ public:
 class Water : public Collectable{
 private:
 public:
-	Water(int startX, int startY) : Collectable(true, IID_WATER_POOL, startX, startY) {
+	Water(StudentWorld*& world, int startX, int startY, Direction dir = right, double size = 1.0, unsigned depth = 2.0, int hp = 1, int strength = 0, int col_range = 3, int detect_range = 9999) : Collectable(world, true, IID_WATER_POOL, startX, startY, dir, size, depth, hp, strength, col_range, detect_range) {
 		existBehavior = std::make_unique<ExistTemporary>();
 	}
 	void doSomething() override;
@@ -318,20 +394,18 @@ class Hazard : public Inanimated{
 private:
 
 public:
-	Hazard(bool visibility, int imgID, int startX, int startY, Direction dir, int dmgDeal = 0) : Inanimated(hazard, visibility, imgID, startX, startY, dir, 1.0, 1) {};
+	Hazard(StudentWorld*& world, bool visibility, int imgID, int startX, int startY, Direction dir, double size = 1.0, unsigned depth = 2.0, int hp = 1, int strength = 0, int col_range = 0, int detect_range = 9999) : Inanimated(world, hazard, visibility, imgID, startX, startY, dir, 1.0, 1, hp, strength, col_range, detect_range) {};
 	virtual ~Hazard() {};
 };
 
 class Squirt : public Hazard{
 private:
-	int ammo;
 	//Functions
-	int getAmmo();
 	void increaseAmmo(int amount);
 	void shoot();
 	void doSomething() override;
 public:
-	Squirt(int startX, int startY, Direction dir, int strength = 2) : Hazard(true, IID_WATER_SPURT, startX, startY, dir, strength) {};
+	Squirt(StudentWorld*& world, int startX, int startY, Direction dir, double size = 1.0, unsigned depth = 2.0, int hp = 1, int strength = 2, int col_range = 4, int detect_range = 9999) : Hazard(world, true, IID_WATER_SPURT, startX, startY, dir, size, depth, hp, strength, col_range, detect_range) {};
 
 };
 
@@ -341,11 +415,11 @@ public:
 private:
 	BoudlderState state = stable;
 	//Functions
-	void falling();
+	void fall();
 	bool checkIceBelow();
 	void doSomething() override;
 public:
-	Boulder(int startX, int startY, Direction dir = down, int strength = 9999) : Hazard(true, IID_BOULDER, startX, startY, dir, strength){
+	Boulder(StudentWorld*& world, int startX, int startY, Direction dir, double size = 1.0, unsigned depth = 2.0, int hp = 1, int strength = 9999, int col_range = 3, int detect_range = 9999) : Hazard(world, true, IID_BOULDER, startX, startY, dir, size, depth, hp, strength, col_range, detect_range){
 		movementBehavior = std::make_unique<FallMovement>(); 
 	}
 };
@@ -354,7 +428,7 @@ class Ice : public Inanimated{
 private:
 	
 public:
-	Ice(bool visibility, int startX, int startY, Direction dir = right, int hp = 1) : Inanimated(ice, visibility, IID_ICE, startX, startY, dir, 0.25, 3, hp) {};
+	Ice(StudentWorld*& world, bool visibility, int startX, int startY, Direction dir = right, double size = 1.0, unsigned depth = 3.0, int hp = 1, int strength = 0, int col_range = 1, int detect_range = 9999) : Inanimated(world, ice, visibility, IID_ICE, startX, startY, dir, 0.25, 3, hp, strength, col_range, detect_range) {};
 	void doSomething() override;
 };
 
